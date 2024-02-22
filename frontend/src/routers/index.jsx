f5975dba09f6e570ef2d01a2e58b0132a02b8d8a
ZhidanRn <zhidanir@gmail.com>
2024-02-22 13:25:50 +0700

    feat: fe - menambahkan Login

--- frontend/src/routers/index.jsx
+++ frontend/src/routers/index.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Homepage from "../pages";
+import Login from "../pages/login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage/>,
+  },
+  {
+    path: "login",
+    element: <Login />,
   },
 ]);
