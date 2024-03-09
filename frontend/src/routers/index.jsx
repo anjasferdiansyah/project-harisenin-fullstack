ff094861ca1519b55534d676722fdbf0887b9ff9
nandap1304 <nandap1304@gmail.com>
2024-03-09 21:16:29 +0700

    feat:FE - page checkout

--- frontend/src/routers/index.jsx
+++ frontend/src/routers/index.jsx
@@ -3,8 +3,13 @@ import Homepage from "../pages";
 import Login from "../pages/login";
 import Register from "../pages/register";
 import ListProducts from "../pages/ListProduct";
+import Checkout from "../pages/Checkout";
 
 export const router = createBrowserRouter([
+  {
+    path: "/checkout",
+    element: <Checkout/>,
+  },
   {
     path: "/",
     element: <Homepage />,
