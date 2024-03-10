aee37ee763f9b6b95bf2a3aa121a0cf663aadb51
anjasferdiansyah <anjasferdiansyah123@gmail.com>
2024-03-10 09:17:07 +0700

    fix: fe - minor bug in cart component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
       futura: ["Futura", "sans-serif"],
     },
+    keyframes: {
+      slideIn: {
+        "0%": { transform: "translateX(100%)" },
+        "100%": { transform: "translateX(0)" },
+      },
+      slideOut: {
+        "0%": { transform: "translateX(0)" },
+        "100%": { transform: "translateX(100%)" },
+      },
+    },
+    animation: {
+      "slide-in": "slideIn 0.3s ease-in-out",
+      "slide-out": "slideOut 0.5s ease-in-out",
+    },
   },
   plugins: [],
-}
-
+};
