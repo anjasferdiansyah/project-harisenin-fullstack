b555abc01cba7f05bca3a4a2aebddf995a3212bf
anjasferdiansyah <anjasferdiansyah123@gmail.com>
2024-04-28 02:12:01 +0700

    fix typo

--- backend/src/routes/productRouter.js
+++ backend/src/routes/productRouter.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-router.get('/', productController.getAllProcuts);
+router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.post('/', productController.createProduct);
 router.delete('/:id', productController.deleteProductById);
 router.patch('/:id', productController.updateProduct);
 
-module.exports = router
+module.exports = router;
