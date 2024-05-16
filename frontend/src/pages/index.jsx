73e4c935e66b24237c8acdff3f5c12b39c284dbe
anjasferdiansyah <anjasferdiansyah123@gmail.com>
2024-05-16 19:55:01 +0700

    fe : fix - refactoring

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,6 +1,6 @@
 import HeroSection from "../components/molecules/HeroSection";
 import Categories from "../components/organisems/Categories";
-import PopularCategory from "../components/organisems/PopularCategory";
+// import PopularCategory from "../components/organisems/PopularCategory";
 import Products from "../components/organisems/Products";
 import HomeLayouts from "../layouts/HomeLayouts";
 
