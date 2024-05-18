fd96db51798815ac10f0d9c5bd39a276f4311fc1
anjasferdiansyah <anjasferdiansyah123@gmail.com>
2024-05-18 23:35:12 +0700

    fe : fix - detailProduct

--- frontend/src/pages/detailProductPage.jsx
+++ frontend/src/pages/detailProductPage.jsx
@@ -9,11 +9,10 @@ import "swiper/css/navigation";
 import "swiper/css/thumbs";
 
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
-import Footer from "../components/organisems/Footer";
-import Navbar from "../components/organisems/Navbar";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
+import HomeLayouts from "../layouts/HomeLayouts";
 
 const DetailProductPage = ({ userId }) => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
@@ -42,16 +41,16 @@ const DetailProductPage = ({ userId }) => {
 
   const onClickAddToCart = () => {
     const token = sessionStorage.getItem("token");
-    if(!token){
+    if (!token) {
       setTimeout(() => {
-        toast.error('Please login first')
+        toast.error("Please login first");
       }, 2000);
       return;
     }
     setTimeout(() => {
-      toast.success('Add to cart successfully')
+      toast.success("Add to cart successfully");
     }, 2000);
-    dispatch(addToCart(token, data && data.id, qty))
+    dispatch(addToCart(token, data && data.id, qty));
   };
 
   const incrementQty = () => {
@@ -71,124 +70,123 @@ const DetailProductPage = ({ userId }) => {
   };
 
   return (
-    <div>
-      <ToastContainer />
-      <div className="flex justify-center flex-wrap">
-        <Navbar />
-        <div className="container max-w-sm ">
-          <Swiper
-            style={{
-              "--swiper-navigation-color": "#fff",
-            }}
-            loop={true}
-            spaceBetween={5}
-            navigation={true}
-            thumbs={{
-              swiper:
-                thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
-            }}
-            modules={[FreeMode, Navigation, Thumbs]}
-            className="m-5 rounded"
-          >
-            <SwiperSlide>
-              <img
-                src={data && data.listImage[0]}
-                alt=""
-                className="object-cover"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={data && data.listImage[1]}
-                alt=""
-                className="object-cover"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={data && data.listImage[2]}
-                alt=""
-                className="object-cover"
-              />
-            </SwiperSlide>
-          </Swiper>
-          <Swiper
-            onSwiper={setThumbsSwiper}
-            loop={true}
-            spaceBetween={20}
-            slidesPerView={3}
-            freeMode={true}
-            watchSlidesProgress={true}
-            modules={[FreeMode, Navigation, Thumbs]}
-            className="m-5"
-          >
-            <SwiperSlide>
-              <img
-                src={data && data.listImage[0]}
-                alt="product-images"
-                className="rounded"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={data && data.listImage[1]}
-                alt="product-images"
-                className="rounded"
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src={data && data.listImage[2]}
-                alt="product-images"
-                className="rounded"
-              />
-            </SwiperSlide>
-          </Swiper>
-        </div>
-        <div className="p-5 md:max-w-xl max-w-sm">
-          <div className="text-4xl font-bold text-blue-900">
-            {data && data.title}
+    <HomeLayouts>
+      <div>
+        <ToastContainer />
+        <div className="flex justify-center flex-wrap">
+          <div className="container max-w-sm ">
+            <Swiper
+              style={{
+                "--swiper-navigation-color": "#fff",
+              }}
+              loop={true}
+              spaceBetween={5}
+              navigation={true}
+              thumbs={{
+                swiper:
+                  thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+              }}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="m-5 rounded"
+            >
+              <SwiperSlide>
+                <img
+                  src={data && data.listImage[0]}
+                  alt=""
+                  className="object-cover"
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img
+                  src={data && data.listImage[1]}
+                  alt=""
+                  className="object-cover"
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img
+                  src={data && data.listImage[2]}
+                  alt=""
+                  className="object-cover"
+                />
+              </SwiperSlide>
+            </Swiper>
+            <Swiper
+              onSwiper={setThumbsSwiper}
+              loop={true}
+              spaceBetween={20}
+              slidesPerView={3}
+              freeMode={true}
+              watchSlidesProgress={true}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="m-5"
+            >
+              <SwiperSlide>
+                <img
+                  src={data && data.listImage[0]}
+                  alt="product-images"
+                  className="rounded"
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img
+                  src={data && data.listImage[1]}
+                  alt="product-images"
+                  className="rounded"
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img
+                  src={data && data.listImage[2]}
+                  alt="product-images"
+                  className="rounded"
+                />
+              </SwiperSlide>
+            </Swiper>
           </div>
-          <div className="text-xl font-bold">
-            Rp{data && data.price.toLocaleString("id-ID")}
-          </div>
-          <div className="flex gap-2 items-center">
-            <div className="h-10 flex my-2 border border-black w-max rounded-md">
-              <i
-                onClick={decrementQty}
-                id="decrement"
-                className="fa-solid fa-minus mx-4 border-gray-700 flex items-center justify-center cursor-pointer"
-              ></i>
-              <input
-                type="text"
-                className="w-14 text-center"
-                id="inputQty"
-                value={qty}
-                onChange={onChangeQty}
-              />
-              <i
-                onClick={incrementQty}
-                id="increment"
-                className="fa-solid fa-plus mx-4 border-gray-100 flex items-center justify-center cursor-pointer"
-              ></i>
+          <div className="p-5 md:max-w-xl max-w-sm">
+            <div className="text-4xl font-bold text-blue-900">
+              {data && data.title}
             </div>
-            <div>
-              <button
-                onClick={onClickAddToCart}
-                className=" bg-blue-900 text-white w-full p-2 rounded font-semibold hover:bg-white hover:text-blue-800 border hover:scale-105"
-              >
-                Add to cart
-              </button>
+            <div className="text-xl font-bold">
+              Rp{data && data.price.toLocaleString("id-ID")}
             </div>
+            <div className="flex gap-2 items-center">
+              <div className="h-10 flex my-2 border border-black w-max rounded-md">
+                <i
+                  onClick={decrementQty}
+                  id="decrement"
+                  className="fa-solid fa-minus mx-4 border-gray-700 flex items-center justify-center cursor-pointer"
+                ></i>
+                <input
+                  type="text"
+                  className="w-14 text-center"
+                  id="inputQty"
+                  value={qty}
+                  onChange={onChangeQty}
+                />
+                <i
+                  onClick={incrementQty}
+                  id="increment"
+                  className="fa-solid fa-plus mx-4 border-gray-100 flex items-center justify-center cursor-pointer"
+                ></i>
+              </div>
+              <div>
+                <button
+                  onClick={onClickAddToCart}
+                  className=" bg-blue-900 text-white w-full p-2 rounded font-semibold hover:bg-white hover:text-blue-800 border hover:scale-105"
+                >
+                  Add to cart
+                </button>
+              </div>
+            </div>
+            <div className="text-sm ">Detail</div>
+            <div className="text-sm ">{data && data.desc}</div>
           </div>
-          <div className="text-sm ">Detail</div>
-          <div className="text-sm ">{data && data.desc}</div>
         </div>
       </div>
-      <Footer />
-    </div>
+    </HomeLayouts>
   );
-
 };
 
-export default DetailProductPage
+export default DetailProductPage;
